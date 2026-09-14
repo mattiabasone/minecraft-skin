22242f112cfcf08655d383c212d1662b63f85abf
Mattia Basone <mattia.basone@gmail.com>
2026-09-15 00:30:01 +0200

    isometric head generato improvements

--- tests/Isometric/HeadTest.php
+++ tests/Isometric/HeadTest.php
@@ -34,8 +34,7 @@ class HeadTest extends BaseTestCase
 
         $actualHead = new \Imagick(self::getHeadSkinPath($username, $size));
 
-        $result = $actualHead->compareImages($head, \Imagick::METRIC_MEANABSOLUTEERROR);
-        $similarity = $result[1];
+        [$_, $similarity] = $actualHead->compareImages($head, \Imagick::METRIC_MEANABSOLUTEERROR);
 
         self::assertLessThan(0.1, $similarity);
     }
@@ -47,6 +46,7 @@ class HeadTest extends BaseTestCase
     {
         return [
             ['_Cyb3r', 32],
+            ['_Cyb3r', 512],
             ['MHF_Steve', 48],
             ['SteveWithHelmet', 64],
         ];
